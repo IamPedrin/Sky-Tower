cfeac058d67b05f2a1dd5581e0b6d2f601cf6600
Pedro Luiz <pedrin.luiz22@gmail.com>
2023-12-12 01:50:50 -0300

    Troca de musica com sucesso do menu para o jogo

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -6,71 +6,77 @@ using UnityEngine.UI;
 
 public class AudioManager : MonoBehaviour
 {
-    public static AudioManager AudioManagerInstance {get; private set; }
+    public static AudioManager AudioManagerInstance { get; private set; }
 
     Slider audioSliderVolume;
     AudioSource audioSourceBG;
 
-    // Start is called before the first frame update
+    // Adicione uma variável para armazenar a música da cena
+    public AudioClip[] sceneMusic;
 
-    private void Awake(){
-        if(AudioManagerInstance == null){
-            AudioManagerInstance = FindObjectOfType<AudioManager>();
+    private void Awake()
+    {
+        if (AudioManagerInstance == null)
+        {
+            AudioManagerInstance = this;
             DontDestroyOnLoad(gameObject);
-        }else{
+        }
+        else
+        {
             Destroy(gameObject);
         }
     }
 
-    void Start(){
+    void Start()
+    {
         SceneManager.sceneLoaded += OnSceneLoad;
         audioSourceBG = GetComponent<AudioSource>();
 
-        if(PlayerPrefs.HasKey("Volume")){
+        if (PlayerPrefs.HasKey("Volume"))
+        {
             audioSourceBG.volume = PlayerPrefs.GetFloat("Volume");
-        }else{
-            audioSourceBG.volume = 0.5f; 
+        }
+        else
+        {
+            audioSourceBG.volume = 0.5f;
             PlayerPrefs.SetFloat("Volume", audioSourceBG.volume);
         }
     }
 
-    void OnSceneLoad(Scene scene, LoadSceneMode sceneMode){
-        if(scene.buildIndex == 1){
+    void OnSceneLoad(Scene scene, LoadSceneMode sceneMode)
+    {
+        // Verifique a cena atual e configure a música conforme necessário
+        if (scene.buildIndex == 1)
+        {
             audioSliderVolume = FindObjectOfType<Slider>();
-            audioSliderVolume.onValueChanged.AddListener(delegate {OnSliderValueChanged();});
+            audioSliderVolume.onValueChanged.AddListener(delegate { OnSliderValueChanged(); });
             audioSliderVolume.value = audioSourceBG.volume;
+        }
+        else if (scene.buildIndex == 2)
+        {
+            // Troque a música da cena
+            ChangeSceneMusic(1);
         }
     }
 
-    // void OnSceneLoad(Scene scene, LoadSceneMode sceneMode)
-    // {
-    //     // Pare a música da cena anterior
-    //     if (currentSceneMusic != null)
-    //     {
-    //         audioSourceBG.Stop();
-    //     }
-
-    //     // Configure a nova música da cena
-    //     if (scene.buildIndex == 1)
-    //     {
-    //         audioSliderVolume = FindObjectOfType<Slider>();
-    //         audioSliderVolume.onValueChanged.AddListener(delegate { OnSliderValueChanged(); });
-    //         audioSliderVolume.value = audioSourceBG.volume;
-    //     }
-    //     else if (scene.buildIndex == 2)
-    //     {
-    //         // Carregue a música da nova cena
-    //         public currentSceneMusic = Resources.Load<AudioClip>("Caminho/Para/Sua/MusicaDaCena2");
-    //         audioSourceBG.clip = currentSceneMusic;
-    //         audioSourceBG.Play();
-    //     }
-    // }
-
-    void OnSliderValueChanged(){
+    void OnSliderValueChanged()
+    {
         print("Mudou o valor do slider!");
         audioSourceBG.volume = audioSliderVolume.value;
         PlayerPrefs.SetFloat("Volume", audioSliderVolume.value);
     }
 
-
+    // Adicione um método para trocar a música da cena
+    public void ChangeSceneMusic(int sceneIndex)
+    {
+        if (sceneIndex >= 0 && sceneIndex < sceneMusic.Length)
+        {
+            audioSourceBG.clip = sceneMusic[sceneIndex];
+            audioSourceBG.Play();
+        }
+        else
+        {
+            Debug.LogError("Índice de cena inválido para troca de música!");
+        }
+    }
 }
