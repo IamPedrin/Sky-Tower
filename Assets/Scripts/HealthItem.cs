257eb0f4cab626bd6258a091db36ce52aee770b2
Pedro Luiz <pedrin.luiz22@gmail.com>
2023-12-11 23:40:59 -0300

    Adiçao de novos inimigos, setando limite do mapa e spawners

--- Assets/Scripts/HealthItem.cs
+++ Assets/Scripts/HealthItem.cs
@@ -1,17 +1,29 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class HealthItem : MonoBehaviour
 {
+    AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+    
+    
     private void OnTriggerEnter2D(Collider2D collision)
     {
+
+
         if (collision.CompareTag("Player"))
         {
             PlayerHealth playerHealth = collision.GetComponent<PlayerHealth>();
 
             if (playerHealth != null && playerHealth.currentHealth < playerHealth.maxHealth)
             {
+                AudioSource.PlayClipAtPoint(audioSource.clip, transform.position);
                 playerHealth.GainHealth();
                 Destroy(gameObject);
             }
